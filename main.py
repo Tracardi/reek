6a046c2c7d1d024c34f6e8010e1cfe6dc98b1ecb
risto <risto.kowaczewski@gmail.com>
2024-04-27 22:59:38 +0200

    Add Progress and Console from rich library and refactor print statements to use rich console in main.py. Add new folders and constraints in config.yaml. Add rich library to requirements.txt.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from iterator import find_reeks
 from utils.converter import file_path_to_module
 from utils.yaml_reader import read_yaml
 
+from rich.progress import Progress
+from rich.console import Console
 
 def _get_allowed(namespace_config: dict):
     return [c for c in namespace_config.get('allowed', [])]
@@ -29,24 +31,45 @@ def get_not_allowed(file: str, namespace, imports) -> List[str]:
     return not_allowed
 
 
+def print_bullet_list(items, bullet="•"):
+    """
+    Prints a list of items with bullet points.
+
+    Args:
+        items (list): A list of items to print.
+        bullet (str): The character to use as the bullet point. Default is a bullet (•).
+    """
+    for item in items:
+        console.print(f"{bullet} {item}")
+
 # Example usage:
 file_path = 'config.yaml'
 config = read_yaml(file_path)
-print(config)
+# print(config)
 
+console = Console()
+console.print("Python module constrain validator (REEK)", style="bold")
 report = defaultdict(list)
 if 'folders' in config:
-    for folder in config['folders']:
-        # Iterate folders
-        for file, imports in find_reeks(folder):
-            constrains = config['constrains']
-            for namespace, namespace_config in constrains.items():
-                not_allowed = get_not_allowed(file, namespace, imports)
-                if not_allowed:
-                    report[namespace].extend(not_allowed)
+    with Progress() as folder_progress:
+        total_folders = len(config['folders'])
+        task = folder_progress.add_task("[gray]Scanning folders...", total=total_folders)
+        for folder in config['folders']:
+            # Iterate folders
+            for file, imports in find_reeks(folder):
+                constrains = config['constrains']
+                for namespace, namespace_config in constrains.items():
+                    not_allowed = get_not_allowed(file, namespace, imports)
+                    if not_allowed:
+                        report[namespace].extend(not_allowed)
 
+            folder_progress.update(task, advance=1)
+
+console.print("Report", style="bold")
 for namespace, validations in report.items():
     if validations:
-        print(f"Set constrain for {namespace} validated in the following files:")
-        for file in validations:
-            print(f" - {file}")
+        validations = list(set(validations))
+        console.rule(f"Constrain failed for namespace \"{namespace}\" ({len(validations)})", style="bold red")
+        console.print(f"List of files that validate the constraints", style="red")
+        console.rule(style="red")
+        print_bullet_list(sorted(validations))
